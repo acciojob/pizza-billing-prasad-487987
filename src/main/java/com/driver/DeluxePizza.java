82faa3d6c554de4e9d5d0d5f94a6adfb1d3f50b2
Prasad Yemekar <emekarprasad@gmail.com>
2025-12-10 12:09:30 +0530

    Update DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,25 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
+	 public DeluxePizza(Boolean isVeg) {
+	        super(isVeg);
+	        addExtraCheese();
+	        addExtraToppings();
+	        
+	    }
+	 
+	 @Override
+	    public void addExtraCheese() {
+	        // Do nothing if called again
+	        super.addExtraCheese();
+	    }
 
-    public DeluxePizza(Boolean isVeg) {
-        super(isVeg);
-        // your code goes here
-    }
+	    @Override
+	    public void addExtraToppings() {
+	        // Do nothing if called again
+	        super.addExtraToppings();
+	    }
+	   
+	    
+	    
 }
